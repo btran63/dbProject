8ccbbb93f50681649679fab4353be305fb5cd655
Brian Tran <Brian Tran>
2016-04-14 21:26:47 -0500

    move things around to match the server
    also changed some of the code to fit the redirects on click

--- contact.php
+++ contact.php
@@ -19,36 +19,32 @@
     <br>
 
 <form action ="contact.php" method ="post">
-<ul> 
-<li> 
-<label for ="name">Name</label>
-<input type="text" name="name" id="name" required/>
-</li>
-<li> 
-<label for ="email">Email</label>
-<input type="text" name="email" id="email" required/>
-</li><br>
-<li> 
-<label for ="topic">Topic</label>
-<select> 
-<option value ="schedule">Schedule an Event</option>
-<option value ="support">Technical Support</option>
-<option value ="ticket">Ticket Sales</option>
-<option value ="other">Other</option>
-</select>
-</li><br>
-<li> 
-<label for ="message">Type your message below. <br> 
-If you are reporting technical difficulties, please enter your operating system, browser type/version, and any other information that you think may be helpful.<br></label>
-<textarea name="message" id="message" cols="42" rows="9" required></textarea>
-</li><br>
-<li> 
-<input type="submit" value="Submit" id="email"/>
-</li>
-<ul>
+    <ul> 
+        <li> 
+            <label for ="name">Name</label>
+            <input type="text" name="name" id="name" required/></li>
+        <li> 
+            <label for ="email">Email</label>
+            <input type="text" name="email" id="email" required/></li><br>
+        <li> 
+            <label for ="topic">Topic</label>
+            <select name="topic"> 
+                <option value ="schedule">Schedule an Event</option>
+                <option value ="support">Technical Support</option>
+                <option value ="ticket">Ticket Sales</option>
+                <option value ="other">Other</option></select></li><br>
+        <li> 
+        <label for ="message">Type your message below. <br> 
+        If you are reporting technical difficulties, please enter your operating system, browser type/version, and any other information that you think may be helpful.<br></label>
+        <textarea name="message" id="message" cols="42" rows="9" required></textarea>
+        </li><br>
+        <li> 
+        <input type="submit" value="Submit" id="email"/>
+        </li>
+    <ul>
 </form>
             <ul id="contact">
-            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xal1/v/l/t1.0-9/12313556_1041753215868752_6695584085920213377_n.jpg?oh=661313cfd1af0761321f1e10db5be913&oe=578720F8"><img src="12313556_1041753215868752_6695584085920213377_n.jpg"></a>
+            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xal1/v/l/t1.0-9/12313556_1041753215868752_6695584085920213377_n.jpg?oh=661313cfd1af0761321f1e10db5be913&oe=578720F8"><img src="./12313556_1041753215868752_6695584085920213377_n.jpg"></a>
                     <ul>
                         <li><a href="#Kent Taylor">Kent Taylor</a></li>
                         <li><a href="#Phone Number">Phone Number: XXX-XXX-XXXX</a></li>
@@ -56,7 +52,7 @@ If you are reporting technical difficulties, please enter your operating system,
                     </ul>
             </li>
             <br>
-            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/11150526_919306578133416_8461172475397297888_n.jpg?oh=c605deecea03d8e4c8e68e0659e01bd2&oe=57730970"><img src="11150526_919306578133416_8461172475397297888_n.jpg"></a>
+            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/11150526_919306578133416_8461172475397297888_n.jpg?oh=c605deecea03d8e4c8e68e0659e01bd2&oe=57730970"><img src="./11150526_919306578133416_8461172475397297888_n.jpg"></a>
                     <ul>
                         <li><a href="#Aaron Tedone">Aaron Tedone</a></li>
                         <li><a href="#Phone Number">Phone Number: XXX-XXX-XXXX</a></li>
@@ -64,7 +60,7 @@ If you are reporting technical difficulties, please enter your operating system,
                     </ul>
             </li>
             <br>
-            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/11091159_10152615891056152_8911970729619676855_n.jpg?oh=9e5355283eba177a334e86693d4bac65&oe=577A0363"><img src="11091159_10152615891056152_8911970729619676855_n.jpg"></a>
+            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/11091159_10152615891056152_8911970729619676855_n.jpg?oh=9e5355283eba177a334e86693d4bac65&oe=577A0363"><img src="./11091159_10152615891056152_8911970729619676855_n.jpg"></a>
                     <ul>
                         <li><a href="#Shah Zaib">Shah Zaib</a></li>
                         <li><a href="#Phone Number">Phone Number: XXX-XXX-XXXX</a></li>
@@ -72,7 +68,7 @@ If you are reporting technical difficulties, please enter your operating system,
                     </ul>
             </li>
             <br>
-            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xfp1/v/t1.0-0/p206x206/301368_258062200905184_825249961_n.jpg?oh=05127946584a472fe7cb1831bdd0f014&oe=5775A891"><img src="301368_258062200905184_825249961_n.jpg"></a>
+            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xfp1/v/t1.0-0/p206x206/301368_258062200905184_825249961_n.jpg?oh=05127946584a472fe7cb1831bdd0f014&oe=5775A891"><img src="./301368_258062200905184_825249961_n.jpg"></a>
                     <ul>
                         <li><a href="#Brian Tran">Brian Tran</a></li>
                         <li><a href="#Phone Number">Phone Number: XXX-XXX-XXXX</a></li>
@@ -80,7 +76,7 @@ If you are reporting technical difficulties, please enter your operating system,
                     </ul>
             </li>
             <br>            
-            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xat1/v/t1.0-9/11665566_10152942033142967_2176484849873132696_n.jpg?oh=a260ee6e1ef57fe1acf50bc94ff2a689&oe=5786D568"><img src="11665566_10152942033142967_2176484849873132696_n.jpg"></a>
+            <li><a href="https://scontent.fhou1-2.fna.fbcdn.net/hphotos-xat1/v/t1.0-9/11665566_10152942033142967_2176484849873132696_n.jpg?oh=a260ee6e1ef57fe1acf50bc94ff2a689&oe=5786D568"><img src="./11665566_10152942033142967_2176484849873132696_n.jpg"></a>
                     <ul>
                         <li><a href="#Andrew Tang">Andrew Tang</a></li>
                         <li><a href="#Phone Number">Phone Number: XXX-XXX-XXXX</a></li>
@@ -93,8 +89,6 @@ If you are reporting technical difficulties, please enter your operating system,
 </html>
 
 <?php
-if ($_POST[ContactUs.html]){
-
 //STORE TEXT VALUES IN VARIABLES
 $name = $_POST['name'];
 $email = $_POST['email'];
