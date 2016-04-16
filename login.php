5f26d52dfeef8c9dc6d84875adb4d35bfd9d2302
shahzaib92 <market.techfx@gmail.com>
2016-04-16 11:44:40 -0500

    Update login.php
    updated login.php

--- login.php
+++ login.php
@@ -42,7 +42,22 @@
     $rows = $stmt->fetch();
 
 
-    if($rows!=0){
+    if ($row != '0'){ 
+	//Need to create ForgotUsernameORPass.html, ForgotUsernameORPass.php is uploaded
+		die ("Inavlid combination of Username and Password! Please try again <a href ='LogIn.html'> &larr; Back</a><br> <a href ='ForgotUsernameOrPass.html'>Forgot your password? </a>");
+	} else{
+	
 
-    }
+	//CHECK IF COACH OR ADMIN
+	if ($row['coach'] == 1 || $row['coach'] == "1"){
+		echo "You are now logged in, Coach.";
+	//create cookies? do coach things and stuff
+	//load coach panel form
+	} else if ($row['admin'] == 1 || $row['admin'] == "1"){
+		echo "Welcome, Admin.";
+	//create cookies? do admin things and stuff
+	//load admin panel form
+	}
+
+	}
 ?>
