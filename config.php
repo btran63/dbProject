64bc5a321b70b7e16d72034e9402cf04b4e4a4a5
istari1000 <istari1000@gmail.com>
2016-04-17 21:47:09 -0500

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
     $db_user = "Andrew";
     $db_pass = "password";
     $db_name = "gymnastics"//name of db;
-
+    $allowedChars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!#$%&'*+-/=?^_`{|}~";
     $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
