fb09d9539cf61701af212d6181ab0b7a16b87159
Andrew Tang <andrewtangg@yahoo.com>
2016-04-20 16:06:49 -0500

    Changed the navigation menu options from .html to .php

--- MyTeam.php
+++ MyTeam.php
@@ -13,12 +13,12 @@
 <body>
     <header><h1>My Team</h1></header>
 			<ul id="menu">
-			<li><a class="active" href="Homepage.html">Home</a></li>
-			<li><a href="logIn.html">Login</a></li>
-			<li><a href="Registration.html">Registration</a></li>
-			<li><a href="Contact.html">Contact</a></li>
-			<li><a href="myteam.html">MyTeam</a><li>
-            <a href="Report.html">Report</a>
+			<li><a class="active" href="Homepage.php">Home</a></li>
+			<li><a href="logIn.php">Login</a></li>
+			<li><a href="Registration.php">Registration</a></li>
+			<li><a href="Contact.php">Contact</a></li>
+			<li><a href="myteam.php">MyTeam</a><li>
+            <a href="Report.php">Report</a>
 			</ul>
 			<br><br>
 			
