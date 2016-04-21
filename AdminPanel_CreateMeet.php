22edd201298ec453e5b6ac7289b4d95f8b9b02cd
istari1000 <istari1000@gmail.com>
2016-04-21 17:25:15 -0500

    Update AdminPanel_CreateMeet.php

--- AdminPanel_CreateMeet.php
+++ AdminPanel_CreateMeet.php
@@ -10,14 +10,14 @@
 	$zipcode = $_POST['zipcode'];
 	$city = $_POST['city'];
 	$state = $_POST['state'];
-	$street_address =$_POST['streetaddress'];
+	$street_address = $_POST['streetaddress'];
 	
 	//CHECK IF START DATE GREATER THAN CURRENT DATE
 	$date_now = date("Y-m-d");
 	$date_convert = date_format($startdate,"Y/m/d");
 	if ($date_now > $date_convert) 
 	{
-	die ("Inavlid date entered! Please try again <a href ='AdminPanel_CreateEvent.html'> &larr; Back</a><br>");
+		die ("Inavlid date entered! Please try again <a href ='AdminPanel_CreateEvent.html'> &larr; Back</a><br>");
 	}
 
 	//CHECK IF END DATE GREATER THAN START DATE
@@ -25,14 +25,15 @@
 	$enddate_convert = date_format($enddate,"Y/m/d");
 	if ($startdate_convert > $enddate_convert) 
 	{
-	die ("End Date must be greater than Start Date! Please try again <a href ='AdminPanel_CreateEvent.html'> &larr; Back</a><br>");
+		die ("End Date must be greater than Start Date! Please try again <a href ='AdminPanel_CreateEvent.html'> &larr; Back</a><br>");
 	}	
 	
 
 	//VERIFY IF MEET NAME ALREADY EXISTS ON THE GIVEN START DATE
 	//MEET NAME CAN HAVE DUPLICATES, FOR EXAMPLE ANNUAL EVENTS
 		$stmt = $db -> prepare("SELECT meet_name FROM Meets WHERE meet_name= ? AND start_date =?");
-		$stmt->execute(array($meetname,$startdate));
+		$params = array(':meet_name' => $meetname, ':start_date' => $startdate);
+		$stmt->execute($params);
 		$row = $stmt->fetch();
 	if ($row != '0'){
 		die ("The Meet Name scheduled for the given date already exists. Try a different Name or Start Date instead! <a href ='AdminPanel_CreateMeet.html'> &larr; Back</a>");
@@ -44,14 +45,13 @@
 	}
 	
 	//CREATE NEW MEET
-	mysql_query("INSERT INTO 'Meets' (meet_name, start_date, end_date, capacity, zip_code, city, state, street_address) VALUES ('$meetname' , '$startdate_convert', '$enddate_convert', $capacity,'$zip_code', '$city', '$state', '$street_address')");
+	$query = $db->prepare("INSERT INTO Meets (meet_name, start_date, end_date, capacity, zip_code, city, state, street_address) VALUES (?, ?, ?, ?, ?, ?, ?, ?");
+	$query->execute(array($meetname , $startdate_convert, $enddate_convert, $capacity, $zip_code, $city, $state, $street_address));
 	
 	//SEND EMAIL TO ALL COACHES ABOUT THE NEW MEET
-	$sql = mysql_query("select email from Coaches");
-	$recipients = array();
-	while($row = mysql_fetch_array($sql)) {
-    	$recipients[] = $row['email'];
-	}
+	$query = $db->prepare("SELECT email FROM Coaches");
+	$query->execute();
+	$recipients = $query->fetchAll();
 	$to = 'nationalgymmeet@gmail.com';
 	$subject = "New Meet: '$meetname' Has Been Scheduled!";
 	$body = "Hello! A new meet has been scheduled and is now available for registeration for your team!";
