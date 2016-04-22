abcc910fe332b4812f921fbbbdaf3f7405c550fc
KTaylorIII <KTaylorIII@users.noreply.github.com>
2016-04-22 11:47:13 -0500

    Update login_views.php

--- login_views.php
+++ login_views.php
@@ -3,6 +3,20 @@ include 'config.php';
 
 function userExists($username){
     $db = connect();
+    $statement = $db->prepare('SELECT username FROM Users WHERE username = :username');
+    $params = array(':username' => $username);
+    $results = $statement->execute($params); // Retrieving the results list here.
+    $db = NULL; // Closing the connection
+    if ($results->rowCount()){
+        return True;
+    }
+    else{
+        return False;
+    }
 }
-function login($username, $password)
+function login($username, $password){
+    if (userExists($username)){
+        $db = connect();
+    }
+}
 ?>
