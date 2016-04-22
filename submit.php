2508d93b37bfd1f9c533b4404be195c872ea9e0c
istari1000 <istari1000@gmail.com>
2016-04-22 16:16:57 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -5,28 +5,32 @@
 	if ($_POST['Registration.html']){
 		
 	//SAVE TEXT VALUES IN VARIABLES
+	$username = $_POST['username'];
 	$firstname = $_POST['userfirstname'];
 	$lastname = $_POST['userlastname'];
 	$teamname = $_POST['teamname'];
-	$usersex = $_POST['usersex'];
 	$phone = $_POST['userphone'];
 	$usermail = $_POST['usermail'];
 	$password = $_POST['password'];
 	$confirmpassword = $_POST['confirmpassword'];
 	
 	//VERIFY USERNAME LENGTH
-	if (strlen($usermail) > 31){
+	if (strlen($username) > 31){
 	die ("Username must not contain more than 31 characters. Please try again! <a href ='Registration.html'> &larr; Back</a>");
 	}
 	
 	//VERIFY IF USER NAME ALREADY EXISTS
-	$stmt = $db -> prepare("SELECT username FROM Users WHERE username= $usermail;");
-	$stmt->execute(array($usermail));
+	$stmt = $db -> prepare("SELECT username FROM Users WHERE username= ?;");
+	$stmt->execute($username);
 	$row = $stmt->fetch();
 	if ($row != '0'){
 	die ("That username already exists. Try a different username instead! <a href ='Registration.html'> &larr; Back</a>");
 	}
-
+	//VERIFY THAT EMAIL DOES NOT EXIST YET
+	$stmt = $db -> prepare("SELECT email FROM Users WHERE email = ?");
+	$stmt -> execute($usermail);
+	if ($row != '0')
+		die ("That email already exists. Use another <a href = 'Registration.html'> &larr; Back</a>");
 	//VERIFY IF TEAM NAME ALREADY EXISTS
 	$stmt = $db -> prepare("SELECT team_name FROM Teams WHERE team_name= $teamname;");
 	$stmt->execute(array($teamname));
@@ -40,22 +44,17 @@
 	if ($password != $confirmpassword){
 	die ("The passwords do not match. Please try again! <a href ='Registeration.html'> &larr; Back</a>");
 	}
-
-	//VERIFY SEX IN M, F, O
-	if ($usersex != "M" || $usersex != "m" || $usersex != "F" || $usersex != "f" || $usersex != "O" || $usersex != "o") {
-		die ("Invalid user gender. Please type one of the following! (M,F,O) <a href ='Registration.html'> &larr; Back</a>");
-	}
-	
 	//CREATE NEW COACH (Will have to update team_id when the team is created?)
-	mysql_query("INSERT INTO 'Coaches' (first_name, last_name, sex, email, phone) VALUES ('$firstname' , '$lastname', '$usersex', '$useremail','$userphone')");
-
-
-	//CREATE NEW COACH_USER (I don't know how to hash?)
-	mysql_query("INSERT INTO 'Users' (username, hash, coach) VALUES ('$usermail' , '$password', '1')");
-
+	$stmt = $db -> prepare("INSERT INTO Coaches(first_name, last_name, sex, email, phone) VALUES(:firstName, :lastName, :userSex, :email, :phone)");
+	$stmt->execute(array("firstName" => $firstname, "lastName" => $lastname, "userSex" => $usersex, "email" => $useremail, "phone" => $userphone));
+	//CREATE NEW COACH_USER 
+	$hash = password_hash($password, PASSWORD_BCRYPT);
+	$stmt = $db -> prepare("INSERT INTO Users (uuid, username, hash, coach, email) VALUES (uuid(), :user, :password, int, :email)");
+	$stmt -> execute(array("user" => $username, "email" => $usermail, "password" => $hash, "int" => '1'));
 	//CREATE NEW TEAM (Need coach ID?)
 	mysql_query("INSERT INTO 'Teams' (team_name) VALUES ('$teamname')");
-
+	$stmt = $db -> prepare("INSERT INTO Team (team_name) VALUES (:team)");
+	$stmt -> execute("team" => $teamname);
 	die("Your account has been created!");
 	}
 	
